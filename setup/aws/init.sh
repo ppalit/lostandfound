e4c004cb275f67afd03c3ef10d3cdab1d10a7985
ppalit <priyabrata.palit@gmail.com>
2015-03-02 23:21:20 -0500

    aws setup automation

---
+++ setup/aws/init.sh
@@ -0,0 +1,3 @@
+
+key='/Users/priyabrata/Documents/aws-keypair.pem'
+ssh -i $key ec2-user@ec2-52-11-56-54.us-west-2.compute.amazonaws.com < runSetup.sh
---
+++ setup/aws/runSetup.sh
@@ -0,0 +1,18 @@
+
+sudo su - root
+cd /opt
+mkdir apps
+chown ec2-user:ec2-user apps
+exit
+cd /opt/apps
+mkdir tomcat solr
+cd tomcat
+wget http://mirror.cogentco.com/pub/apache/tomcat/tomcat-8/v8.0.20/bin/apache-tomcat-8.0.20.zip
+unzip apache-tomcat-8.0.20.zip
+chmod 770 apache-tomcat-8.0.20/bin/*.sh
+sh apache-tomcat-8.0.20/bin/startup.sh
+cd ../solr
+wget http://apache.mesi.com.ar/lucene/solr/5.0.0/solr-5.0.0.zip
+unzip solr-5.0.0.zip
+sh solr-5.0.0/bin/solr start
+echo "server setup complete"
