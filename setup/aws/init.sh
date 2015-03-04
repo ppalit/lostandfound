764ffa7d2da1cad2577e1039faef68759bf4681d
ppalit <priyabrata.palit@gmail.com>
2015-03-04 16:43:47 -0500

    changed to static ip

--- setup/aws/init.sh
+++ setup/aws/init.sh
@@ -1,8 +1,8 @@
 echo "Starting setup for AWS instances"
-key='/Users/priyabrata/Documents/aws-keypair.pem'
-instance='ec2-52-11-56-54.us-west-2.compute.amazonaws.com'
+key='/Users/priyabrata/Documents/aws/aws-keypair2.pem'
+instance='54.165.125.72'
 echo "Working with Key=$key  and instance=$instance"
-ssh -i $key ec2-user@ec2-52-11-56-54.us-west-2.compute.amazonaws.com < runSetup.sh
+ssh -i $key ec2-user@$instance < runSetup.sh
 echo "server has been setup!!"
 echo "running tests to check if URLS are reachable.."
 wget --spider -S "http://$instance:8080" 2>&1 | grep "HTTP/" | awk '{print $2}'
