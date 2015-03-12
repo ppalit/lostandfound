ad1c188ba928ed2507ce5c5c2d5f4b09cbdbaf30
ppalit <priyabrata.palit@gmail.com>
2015-03-12 17:28:50 -0400

    Create dummpValues.sql

---
+++ setup/db/scripts/dml/dummpValues.sql
@@ -0,0 +1,4 @@
+insert into reporter values("Priyabrata","Palit","001","priyabrata.palit@gmail.com","8764583648");
+insert into reporter values("Siddhartha","Behera","002","siddharthabehera@gmail.com","784235667425");
+
+insert into item_primary values ("LOST001", "category1","sub_category1", "public_description public_description public_description public_description", "secret_description secret_description",'now',"street_address1 street_address", "68.722885", "-101.09166", "APPROXIMATE", "Edinburgh", "USA", "PA");
