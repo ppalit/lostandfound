7182a014a119e2bd9eb02ce7b83d1fe87d57445b
ppalit <priyabrata.palit@radian.biz>
2015-03-16 17:20:29 -0400

    added firName and LastName

--- common/src/main/java/com/lostandfound/common/bean/ReporterBean.java
+++ common/src/main/java/com/lostandfound/common/bean/ReporterBean.java
@@ -5,40 +5,13 @@ import java.io.Serializable;
 public class ReporterBean implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-	private String fullName;
 	private String emailId;
 	private String phoneNo;
 	private String userId;
+	private String firstName;
+	private String lastName;
 	
 	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "ReporterBean ["
-				+ (fullName != null ? "fullName=" + fullName + ", " : "")
-				+ (emailId != null ? "emailId=" + emailId + ", " : "")
-				+ (phoneNo != null ? "phoneNo=" + phoneNo + ", " : "")
-				+ (userId != null ? "userId=" + userId : "") + "]";
-	}
-
-
-	/**
-	 * @return the fullName
-	 */
-	public String getFullName() {
-		return fullName;
-	}
-
-
-	/**
-	 * @param fullName the fullName to set
-	 */
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
-
 
 	/**
 	 * @return the emailId
@@ -85,6 +58,30 @@ public class ReporterBean implements Serializable{
 	 */
 	public void setUserId(String userId) {
 		this.userId = userId;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		lastName = lastName;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "ReporterBean ["
+				+ (emailId != null ? "emailId=" + emailId + ", " : "")
+				+ (phoneNo != null ? "phoneNo=" + phoneNo + ", " : "")
+				+ (userId != null ? "userId=" + userId + ", " : "")
+				+ (firstName != null ? "firstName=" + firstName + ", " : "")
+				+ (lastName != null ? "lastName=" + lastName : "") + "]";
 	}
 	
 	
