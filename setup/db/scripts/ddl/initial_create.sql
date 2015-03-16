fe49729382387dc3582554d7502a7f81e7ed59b5
ppalit <priyabrata.palit@gmail.com>
2015-03-16 15:16:24 -0400

    Update initial_create.sql

--- setup/db/scripts/ddl/initial_create.sql
+++ setup/db/scripts/ddl/initial_create.sql
@@ -1,5 +1,5 @@
 create table reporter(first_name varchar(20),last_name varchar(20),user_id varchar(20),email_id varchar(20), phone_no varchar(20));
-create table item_primary(item_id varchar(40),category varchar(20),sub_category varchar(20),public_description varchar(400),secret_description varchar(300), item_found_date datetime default current_timestamp, street_address varchar(100), lat varchar(20), lng varchar(20), loc_type varchar(10), city varchar(50), country varchar(50), state varchar(80));
+create table item_primary(item_id integer primary key autoincrement,category varchar(20),sub_category varchar(20),public_description varchar(400),secret_description varchar(300), item_found_date datetime default current_timestamp, street_address varchar(100), lat varchar(20), lng varchar(20), loc_type varchar(10), city varchar(50), country varchar(50), state varchar(80));
 
 alter table item_primary add colour varchar(100);
 alter table item_primary add reporter_id varchar(20);
