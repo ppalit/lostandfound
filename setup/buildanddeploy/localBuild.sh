03c2a1ffc318a78ec1e8714fdb90903657c49d32
ppalit <priyabrata.palit@gmail.com>
2015-03-21 19:29:24 -0400

    Update localBuild.sh

--- setup/buildanddeploy/localBuild.sh
+++ setup/buildanddeploy/localBuild.sh
@@ -1,5 +1,11 @@
 echo "downloading latest from master.."
-curl -LkSs https://github.com/ppalit/lostandfound/archive/master.zip -o master.zip
+rm -rf master.zip sample-laf-master
+curl -LkSs https://github.com/ppalit/sample-laf/archive/master.zip -o master.zip
 unzip master.zip
-cd lostandfound-master
-mvn clean install
+cd sample-laf-master
+mvn clean install -P $1
+cd ..
+#ssh -i aws-keypair2.pem ec2-user@54.165.125.72
+#cd /opt/apps/
+scp -i aws-keypair2.pem sample-laf-master/webface/target/webface.war ec2-user@54.165.125.72:/opt/apps/tomcat/apache-tomcat-8.0.20/webapps/
+scp -i aws-keypair2.pem sample-laf-master/businessServices/target/business-services.war ec2-user@54.165.125.72:/opt/apps/tomcat/apache-tomcat-8.0.20/webapps/
