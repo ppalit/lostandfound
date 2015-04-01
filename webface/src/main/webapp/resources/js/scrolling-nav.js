22016010ec42e96fe83af05411aa1df62bcaa109
siddhartha behera <siddharthabehera@yahoo.co.in>
2015-04-01 23:25:19 +0100

    SUCCESS, ERROR ON INUPT ADDED

--- webface/src/main/webapp/resources/js/scrolling-nav.js
+++ webface/src/main/webapp/resources/js/scrolling-nav.js
@@ -48,7 +48,7 @@ function sizeContent() {
 
 
 function initTooltip(){
-//	$("[data-toggle ='tooltip']").tooltip({container:'body', trigger:'click'});
+	//$("[data-toggle ='tooltip']").tooltip({container:'body', trigger:'click'});
 }
 
 /**
