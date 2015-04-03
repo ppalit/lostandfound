b9352faf7410f2d4f9630a9ea4ebffff1de40421
ppalit <priyabrata.palit@radian.biz>
2015-04-03 15:01:15 -0400

    added configuration scripts

--- setup/db/scripts/ddl/initial_create.sql
+++ setup/db/scripts/ddl/initial_create.sql
@@ -4,6 +4,7 @@ CREATE TABLE reporter(first_name varchar(20),last_name varchar(20),email_id varc
 CREATE TABLE item_primary(item_id integer primary key autoincrement,category varchar(20),sub_category varchar(20),public_description varchar(400),
 secret_description varchar(300), item_found_date datetime default current_timestamp, item_reported_date datetime default current_timestamp, street_address varchar(100), lat varchar(20), lng varchar(20),
  loc_type varchar(10), city varchar(50), country varchar(50), state varchar(80),reporter_id varchar(40), colour varchar(100),FOREIGN KEY(reporter_id) REFERENCES reporter(email_id));
+create TABLE APP_CONFIG (config_id integer primary key autoincrement, config_type varchar(50), config_parent integer, config_key varchar(50), config_value varchar(50),order_num integer );
 COMMIT;
 
 
