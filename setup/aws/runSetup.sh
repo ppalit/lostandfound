f16536a4f1870d9f42f021b0c9c297dc60616b7a
ppalit <priyabrata.palit@gmail.com>
2015-05-26 16:22:03 -0400

    Update runSetup.sh

--- setup/aws/runSetup.sh
+++ setup/aws/runSetup.sh
@@ -7,10 +7,10 @@ exit
 cd /opt/apps
 mkdir tomcat solr
 cd tomcat
-wget http://mirror.cogentco.com/pub/apache/tomcat/tomcat-8/v8.0.20/bin/apache-tomcat-8.0.20.zip
-unzip apache-tomcat-8.0.20.zip
-chmod 770 apache-tomcat-8.0.20/bin/*.sh
-sh apache-tomcat-8.0.20/bin/startup.sh
+wget http://mirror.cogentco.com/pub/apache/tomcat/tomcat-8/v8.0.22/bin/apache-tomcat-8.0.22.zip
+unzip apache-tomcat-8.0.22.zip
+chmod 770 apache-tomcat-8.0.22/bin/*.sh
+sh apache-tomcat-8.0.22/bin/startup.sh
 cd ../solr
 wget http://apache.mesi.com.ar/lucene/solr/5.0.0/solr-5.0.0.zip
 unzip solr-5.0.0.zip
